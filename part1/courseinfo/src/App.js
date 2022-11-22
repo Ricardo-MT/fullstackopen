fb839c286aad62f456db359453d47309710e40a8
Ricardo Mejias Travieso <63120247+Ricardo-MT@users.noreply.github.com>
2022-11-22 13:39:15 +0100

    1.5
    Change the course and its parts into a single JavaScript object. Fix everything that breaks.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -29,26 +29,29 @@ const Total = ({total}) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
-  const parts = [part1, part2, part3];
 
   return (
     <div>
-      <Header course={course} />
-      <Content sections={parts}/>
-      <Total total={parts.map(p => p.exercises).reduce((p, c) => p + c)}/>
+      <Header course={course.name} />
+      <Content sections={course.parts}/>
+      <Total total={course.parts.map(p => p.exercises).reduce((p, c) => p + c)}/>
     </div>
   );
 }
