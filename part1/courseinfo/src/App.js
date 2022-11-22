88e4a69bc64d1cfc673b8ecb7200cef9c5d40b9c
Ricardo Mejias Travieso <63120247+Ricardo-MT@users.noreply.github.com>
2022-11-22 13:19:48 +0100

    1.3
    Modify the variable definitions of the App component as follows and also refactor the application so that it still works.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -15,9 +15,9 @@ const Part = ({part, exercises}) => {
 const Content = ({sections}) => {
   return (
     <p>
-      <Part part={sections[0].part} exercises={sections[0].exercises} />
-      <Part part={sections[1].part} exercises={sections[1].exercises} />
-      <Part part={sections[2].part} exercises={sections[2].exercises} />
+      <Part part={sections[0].name} exercises={sections[0].exercises} />
+      <Part part={sections[1].name} exercises={sections[1].exercises} />
+      <Part part={sections[2].name} exercises={sections[2].exercises} />
     </p>
   );
 }
@@ -30,22 +30,28 @@ const Total = ({total}) => {
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  };
 
   return (
     <div>
       <Header course={course} />
       <Content sections={[
-        {part: part1, exercises: exercises1},
-        {part: part2, exercises: exercises2},
-        {part: part3, exercises: exercises3},
+        part1,
+        part2,
+        part3,
         ]}/>
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   );
 }
